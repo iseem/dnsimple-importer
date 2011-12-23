3b67e2a19e38b5ee1f4a7ea9bdf97984d64ae214
Anthony Eden <anthonyeden@gmail.com>
2011-12-23 12:40:41 +0100

    Provide a means to debug. Update to the latest dnsimple-ruby gem.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'polyglot'
 gem 'dns-zonefile', :git => 'git://github.com/aeden/dns-zonefile.git'
 gem 'dnsimple-ruby'
 
+gem 'ruby-debug19'
+
 group :development do
   gem 'rspec'
+  
 end
--- lib/dnsimple/bind/zone_importer.rb
+++ lib/dnsimple/bind/zone_importer.rb
@@ -26,38 +26,11 @@ module DNSimple
         puts "domain name: #{domain.name}"
 
         zone.records.each do |r|
-          case r
-          when DNS::A then
-            puts "A record: #{r.host} -> #{r.address} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'A', r.address, :ttl => r.ttl) 
-          when DNS::AAAA then
-            puts "AAAA record: #{r.host} -> #{r.address} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'AAAA', r.address, :ttl => r.ttl)
-          when DNS::CNAME then
-            puts "CNAME record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'CNAME', r.domainname, :ttl => r.ttl)
-          when DNS::NS then
-            if host_name(r.host, domain.name).blank?
-              puts "Skip NS record for SLD: #{r.host} -> #{r.domainname}"
-            else
-              puts "NS record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
-              DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'NS', r.domainname, :ttl => r.ttl)
-            end
-          when DNS::PTR then
-            puts "PTR record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'CNAME', r.domainname, :ttl => r.ttl)
-          when DNS::MX then
-            puts "MX record: #{r.host} -> #{r.domainname} (prio: #{r.priority}, ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'MX', r.domainname, :ttl => r.ttl, :prio => r.priority)
-          when DNS::TXT then
-            puts "TXT record: #{r.host} -> #{r.data} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'TXT', r.data, :ttl => r.ttl)
-          when DNS::SRV then
-            puts "SRV record: #{r.host} -> #{r.domainname} (prio: #{r.priority}, weight: #{r.weight}, port: #{r.port}, ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'SRV', "#{r.weight} #{r.port} #{r.domainname}", :ttl => r.ttl, :prio => r.priority)
-          when DNS::NAPTR then
-            puts "NAPTR record: #{r.host} -> #{t.data} (ttl: #{r.ttl})"
-            DNSimple::Record.create(domain.name, host_name(r.host, domain.name), 'NAPTR', r.data, :ttl => r.ttl)
+          begin
+            import_record(domain, r) 
+          rescue => e
+            debugger
+            puts "Error importing #{r.host}: #{e.message}"
           end
         end
       end
@@ -69,6 +42,47 @@ module DNSimple
       def extract_name(n)
         n = n.gsub(/\.db/, '')
         n = n.gsub(/\.txt/, '')
+      end
+
+      def clean_name(n)
+        n.gsub(/\.$/, '')
+      end
+
+      def import_record(domain, r)
+        case r
+          when DNS::A then
+            puts "A record: #{r.host} -> #{r.address} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'A', r.address, :ttl => r.ttl) 
+          when DNS::AAAA then
+            puts "AAAA record: #{r.host} -> #{r.address} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'AAAA', r.address, :ttl => r.ttl)
+          when DNS::CNAME then
+            puts "CNAME record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'CNAME', clean_name(r.domainname), :ttl => r.ttl)
+            puts "CNAME record created"
+          when DNS::NS then
+            if host_name(r.host, domain.name).blank?
+              puts "Skip NS record for SLD: #{r.host} -> #{r.domainname}"
+            else
+              puts "NS record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
+              DNSimple::Record.create(domain, host_name(r.host, domain.name), 'NS', clean_name(r.domainname), :ttl => r.ttl)
+            end
+          when DNS::PTR then
+            puts "PTR record: #{r.host} -> #{r.domainname} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'CNAME', clean_name(r.domainname), :ttl => r.ttl)
+          when DNS::MX then
+            puts "MX record: #{r.host} -> #{r.domainname} (prio: #{r.priority}, ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'MX', clean_name(r.domainname), :ttl => r.ttl, :prio => r.priority)
+          when DNS::TXT then
+            puts "TXT record: #{r.host} -> #{r.data} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'TXT', r.data, :ttl => r.ttl)
+          when DNS::SRV then
+            puts "SRV record: #{r.host} -> #{r.domainname} (prio: #{r.priority}, weight: #{r.weight}, port: #{r.port}, ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'SRV', "#{r.weight} #{r.port} #{r.domainname}", :ttl => r.ttl, :prio => r.priority)
+          when DNS::NAPTR then
+            puts "NAPTR record: #{r.host} -> #{t.data} (ttl: #{r.ttl})"
+            DNSimple::Record.create(domain, host_name(r.host, domain.name), 'NAPTR', r.data, :ttl => r.ttl)
+          end
       end
 
     end
